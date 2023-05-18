bd68f9dc00e42486431fb9f74d2711493723862e
Nayan Prajapati <111540429+Nayanp35@users.noreply.github.com>
2023-05-17 23:14:05 -0400

    Update solution.js

--- medium/1750. Minimum Length of String After Deleting Similar Ends/solution.js
+++ medium/1750. Minimum Length of String After Deleting Similar Ends/solution.js
@@ -1,21 +1,16 @@
 function minimumLength(s) {
-  let left = 0;
-  let right = s.length -1;
-  while (left < right) {
-    let leftChar = s[left];
-    let rightChar = s[right];
-    if (leftChar !== rightChar) break;
-    // move the left pointer to the right while the characters are the same
-    while (left < right && leftChar === s[left]) {
-      left++;
+    const n = s.length;
+    let start = 0;
+    let end = n - 1;
+    while (start < end && s[start] === s[end]) {
+        while (start + 1 < end && s[start] === s[start + 1]) {
+            start++;
+        }
+        while (start < end - 1 && s[end] === s[end - 1]) {
+            end--;
+        }
+        start++;
+        end--;
     }
-    // if they meet at the middle of the string, return 0
-    if (left === right) return 0;
-    // move the right pointer to the left while the characters are the same
-    while (left < right && rightChar === s[right]) {
-      right--;
-    }
-  }
-  return right - left + 1;
+    return Math.max(0, end - start + 1);
 }
-

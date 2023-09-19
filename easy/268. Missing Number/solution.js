2e1dd1b4a9aa37e52a7085946575038783cc4b77
Nayankumar Prajapati <111540429+nayanp35@users.noreply.github.com>
2023-09-19 00:34:17 -0400

    Update solution.js

--- easy/268. Missing Number/solution.js
+++ easy/268. Missing Number/solution.js
@@ -1,3 +1,17 @@
+function missingNumber(nums) {
+    let xor = 0;
+    for (let i = 0; i <= nums.length; i++) {
+        xor ^= i;
+    }
+
+    for (const num of nums) {
+        xor ^= num;
+    }
+
+    return xor;
+}
+
+
 var missingNumber = function(nums) {
     const n = nums.length;
     const expectedSum = (n * (n + 1)) / 2;

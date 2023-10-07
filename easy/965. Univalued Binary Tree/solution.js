2893deee205efa676bd8235b1bb40f9730f8936a
Nayankumar Prajapati <111540429+nayanp35@users.noreply.github.com>
2023-10-07 19:13:40 -0400

    Update solution.js

--- easy/965. Univalued Binary Tree/solution.js
+++ easy/965. Univalued Binary Tree/solution.js
@@ -1,5 +1,5 @@
 var isUnivalTree = function(root, rootVal = root.val) {
-        if (!root) return true;
-        if (root.val !== rootVal) return false;
-        return (isUnivalTree(root.left, rootVal) && isUnivalTree(root.right, rootVal))
+    if (!root) return true;
+    if (root.val !== rootVal) return false;
+    return (isUnivalTree(root.left, rootVal) && isUnivalTree(root.right, rootVal))
 };

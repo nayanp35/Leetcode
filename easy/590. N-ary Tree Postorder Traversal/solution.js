39b4386da5020d8274b79c57291cb45b1df443de
Nayankumar Prajapati <111540429+nayanp35@users.noreply.github.com>
2023-10-07 01:10:10 -0400

    Update solution.js

--- easy/590. N-ary Tree Postorder Traversal/solution.js
+++ easy/590. N-ary Tree Postorder Traversal/solution.js
@@ -3,9 +3,11 @@ var postorder = function(root) {
 
     function traverse(node) {
         if (!node) return null;
+        // go to the very last of first child and process from the
         for (const child of node.children) {
             traverse(child);
         }
+        // push the curr node val to the result;
         result.push(node.val)
     }
 

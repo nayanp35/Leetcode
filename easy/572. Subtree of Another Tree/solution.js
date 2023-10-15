0fc1288fb6de3371213c83a2fe5601f393e85dd8
Nayankumar Prajapati <111540429+nayanp35@users.noreply.github.com>
2023-10-15 18:06:29 -0400

    Create solution.js

---
+++ easy/572. Subtree of Another Tree/solution.js
@@ -0,0 +1,12 @@
+const isSubtree = (root, subRoot) => {
+    if (!root) return false;
+    if (isSameTree(root, subRoot)) return true;
+    return (isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot));
+};
+
+const isSameTree = (p, q) => {
+    if (!p && !q) return true;
+    if (!p || !q) return false;
+    if (p.val !== q.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};

1fa7be36d301eed5c07737c2b2cef87dff9f21ad
Nayankumar Prajapati <111540429+nayanp35@users.noreply.github.com>
2023-10-22 18:35:18 -0400

    Update solution.js

--- easy/1572. Matrix Diagonal Sum/solution.js
+++ easy/1572. Matrix Diagonal Sum/solution.js
@@ -16,3 +16,30 @@ var diagonalSum = function(mat) {
     }
     return total;
 };
+
+// Recursively
+
+function diagonalSum(mat) {
+    let len = mat.length;
+    let total = 0;
+
+    total += primaryDiagonal(mat);
+    total += secondaryDiagonal(mat);
+    if (len & 1) {
+        let mid = len >> 1;
+        total -= mat[mid][mid];
+    }
+    return total;
+}
+
+function primaryDiagonal(mat, i = 0, j = 0, total = 0) {
+    if (i >= mat.length) return total;
+    total += mat[i][j] + primaryDiagonal(mat, i + 1, j + 1, total);
+    return total;
+}
+
+function secondaryDiagonal(mat, i = 0, j = mat.length -1, total = 0) {
+    if (i >= mat.length) return total;
+    total += mat[i][j] + secondaryDiagonal(mat, i + 1, j - 1, total);
+    return total;
+}
